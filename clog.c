720e94073f1090e3f04058d9bfaa8dd2190f451c
YMaster <helium747@gmail.com>
2022-04-21 18:44:37 +0800

    [fix] move static variables in header to source file

--- clog.c
+++ clog.c
@@ -4,6 +4,16 @@
 #include <pthread.h>
 #include "clog/clog.h"
 
+static const char *log_level_str[] = {
+        "TRACE", "DEBUG", "INFO", "WARN", "ERROR", "FATAL",
+};
+
+static const char *level_colors[] = {
+        "\x1b[94m", "\x1b[36m", "\x1b[32m", "\x1b[33m", "\x1b[31m", "\x1b[35m"
+};
+
+static const char *color_reset = "\x1b[0m";
+
 static struct {
     bool quiet;
     enum clog_level level;
--- include/clog/clog.h
+++ include/clog/clog.h
@@ -11,16 +11,6 @@ enum clog_level {
     CLOG_LEVEL_FATAL,
 };
 
-static const char *log_level_str[] = {
-        "TRACE", "DEBUG", "INFO", "WARN", "ERROR", "FATAL",
-};
-
-static const char *level_colors[] = {
-        "\x1b[94m", "\x1b[36m", "\x1b[32m", "\x1b[33m", "\x1b[31m", "\x1b[35m"
-};
-
-static const char *color_reset = "\x1b[0m";
-
 #define log_trace(...) clog_log(CLOG_LEVEL_TRACE, __VA_ARGS__)
 #define log_debug(...) clog_log(CLOG_LEVEL_DEBUG, __VA_ARGS__)
 #define log_info(...)  clog_log(CLOG_LEVEL_INFO, __VA_ARGS__)
